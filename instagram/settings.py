76cfd000f4db4f9bd3503185cd4d5a07fa73fc09
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-03 16:29:33 +0300

    adds static files

--- instagram/settings.py
+++ instagram/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'cloudinary',
-    'posts.apps.PostsConfig',
+    'posts',
+    
 ]
 
 MIDDLEWARE = [
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Image(models.Model):
+    img_name = models.CharField(max_length=100, blank=True)
+    image = CloudinaryField('image')
